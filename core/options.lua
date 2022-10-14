3f8c110036493799dec97cf838c665e949856241
Benjamin Zinschitz <benjamin@formunauts.at>
2022-10-14 14:30:57 +0200

    add basic vim options
    Signed-off-by: Benjamin Zinschitz <benjamin@formunauts.at>

---
+++ core/options.lua
@@ -0,0 +1,69 @@
+local options = {
+    -- don't create backup files
+    backup = false,
+    -- creates a swapfile
+    swapfile = false,
+    -- the encoding written to a file
+    fileencoding = "utf-8",
+    -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    writebackup = false,
+    -- enable persistent undo
+    undofile = true,
+    -- reload file changes from disk
+    autoread = true,
+    -- respect file specific vim settings
+    modeline = true,
+    modelines = 10,
+
+    -- allows neovim to access the system clipboard
+    clipboard = "unnamedplus",
+    -- no preselect for autocompletion
+    completeopt = { "menuone", "noselect" },
+    -- so that `` is visible in markdown files
+    conceallevel = 0,
+
+    -- convert tabs to spaces
+    expandtab = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    -- make indenting smarter again
+    smartindent = true,
+
+    -- time to wait for a mapped sequence to complete (in milliseconds)
+    timeoutlen = 200,
+    -- faster completion (4000ms default)
+    updatetime = 300,
+
+    -- highlight the current line
+    cursorline = true,
+    -- show linenumbers
+    number = true,
+    relativenumber = true,
+    -- always show the sign column
+    signcolumn = "yes",
+    -- split directions
+    splitbelow = true,
+    splitright = true,
+    -- more useful searching
+    hlsearch = true,
+    incsearch = true,
+    ignorecase = true,
+    smartcase = true,
+    -- scroll offset
+    scrolloff = 3,
+    sidescrolloff = 3,
+    -- display lines as one long line
+    wrap = false,
+    -- pop up menu height
+    pumheight = 10,
+    -- hide -- INSERT --
+    showmode = false,
+}
+
+-- hide "match xx of xx" and other messages during auto-completion
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
