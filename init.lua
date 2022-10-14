a8cea1494d04130b71ccf358682b9b7526d91563
Benjamin Zinschitz <benjamin@formunauts.at>
2022-10-14 13:12:38 +0200

    define basic project stucture
    Signed-off-by: Benjamin Zinschitz <benjamin@formunauts.at>

---
+++ init.lua
@@ -0,0 +1,27 @@
+local utils = require("utils")
+
+-- check if we have the latest stable version of nvim
+local expected_ver = "0.8.0"
+local nvim_ver = utils.get_nvim_version()
+
+if nvim_ver ~= expected_ver then
+  local msg = string.format("Unsupported nvim version: expect %s, but got %s instead!", expected_ver, nvim_ver)
+  vim.api.nvim_err_writeln(msg)
+  return
+end
+
+local core_conf_files = {
+  "globals.lua",        -- some global settings
+  "options.lua",        -- setting options in nvim
+  "autocommands.lua",   -- various autocommands
+  "keymaps.lua",        -- all the user-defined mappings
+  "plugins.lua",        -- all the plugins installed and their configurations
+  "colorschemes.lua",   -- colorscheme settings
+}
+
+-- source all the core config files
+for _, name in ipairs(core_conf_files) do
+  local path = string.format("%s/core/%s", vim.fn.stdpath("config"), name)
+  local source_cmd = "source " .. path
+  vim.cmd(source_cmd)
+end
